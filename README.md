af246feb62563f77819b1490955b6ec84f0a78dc
Arun Widjaja <awidjaja@greenstreet.com>
2025-12-02 10:32:19 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+2025 Advent of Code Challenge:
+
+https://adventofcode.com/
---
+++ advent_1.py
@@ -0,0 +1,25 @@
+
+STARTING_POSITION = 50
+DIAL_RANGE = 99
+
+with open('advent_1_input.txt', 'r') as file:
+    data = file.read().splitlines()
+for i in range(len(data)):
+    if data[i].startswith('R'):
+        data[i] = int(data[i][1:])
+    elif data[i].startswith('L'):
+        data[i] = int('-' + data[i][1:])
+
+currentNumber = STARTING_POSITION
+timesZero = 0
+
+print('Start dial at', currentNumber)
+for adjustment in data:
+    print(f'Move dial by: {adjustment:>4}')
+    currentNumber += adjustment
+    currentNumber = currentNumber % (DIAL_RANGE + 1)
+    print(f'Dial now at:  {currentNumber:>4}\n')
+    if currentNumber == 0:
+        timesZero += 1
+
+print('Solution:', timesZero)
